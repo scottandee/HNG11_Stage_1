8855678334c6c8b5447e19ccafcaf1c720eefae7
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-07-01 20:25:19 +0100

    Allow port to be substituted

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const requestIp = require('request-ip');
 const dotenv = require('dotenv');
 
 dotenv.config()
-const port = 5000
+const port = process.env.PORT || 5000;
 app.use(requestIp.mw());
 
 app.get('/api/hello/', async (req, res) => {
